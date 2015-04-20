e2695a77ac57baab9c6585cfab50588993c83c98
slin <yangsonglin@lezhai365.com>
2015-04-20 10:01:33 +0800

    初始化工程

---
+++ src/main/java/com/lezhai365/wechat/propertymanager/PropertyManagerController.java
@@ -0,0 +1,16 @@
+package com.lezhai365.wechat.propertymanager;
+
+import com.lezhai365.wechat.controller.BaseController;
+
+/**
+ * @author :  SongLin.Yang [ysl09240@gmail.com]
+ * @version :  1.0
+ * @encoding : UTF-8
+ * @package : com.lezhai365.wechat.controller
+ * @link :  http://lezhai365.com
+ * @created on   :  15-4-20
+ * @copyright :  Copyright(c) 2013 西安乐宅网络科技有限公司
+ * @description :
+ */
+public class PropertyManagerController extends BaseController {
+}
