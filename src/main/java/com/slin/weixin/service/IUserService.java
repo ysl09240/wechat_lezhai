8fd1f5976fe603c582a7c9ed0659b793b0f51fd4
ysl09240 <ysl09240@Gmail.com>
2016-05-04 08:57:31 +0800

    添加pojo的别名文件

---
+++ src/main/java/com/slin/weixin/service/IUserService.java
@@ -0,0 +1,11 @@
+package com.slin.weixin.service;
+
+import com.slin.weixin.pojo.User;
+
+/**
+ * @author SongLin.Yang
+ * @data 2016-04-29 15:08
+ */
+public interface IUserService {
+    public User getUser(int id);
+}
